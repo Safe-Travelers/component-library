691ca81ee0a177e39698426f328f8b40a6c36ff1
James Zhao <accounts.james.zhao@protonmail.com>
2022-12-02 11:36:37 -0500

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 import '@jam-dev/starlight-react/css';
+export * from './component/contact-preview/ContactPreview';
 export * from './component/trip-preview/TripPreview';
+export * from './component/trip-view/TripView';
