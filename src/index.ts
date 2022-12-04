a364229fd14c685484cd859ac9a31fe420fb29e2
Isha Jain <ishajainpdx@gmail.com>
2022-12-04 15:22:29 -0500

    added star rating component, now will add review-preview

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import '@jam-dev/starlight-react/css';
-export * from './component/contact-preview/ContactPreview';
-export * from './component/trip-preview/TripPreview';
-export * from './component/trip-view/TripView';
+import "@jam-dev/starlight-react/css";
+export * from "./component/contact-preview/ContactPreview";
+export * from "./component/trip-preview/TripPreview";
+export * from "./component/trip-view/TripView";
+export * from "./component/star-rating/StarRating";
