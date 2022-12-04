17ab017af23e1da5ff0cdf8b0eae512ea7fde022
James Zhao <accounts.james.zhao@protonmail.com>
2022-12-04 17:22:11 -0500

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import '@jam-dev/starlight-react/css';
 export * from './component/contact-preview/ContactPreview';
+export * from './component/form/form-container/FormContainer';
+export * from './component/form/form-row/FormRow';
+export * from './component/form/form-submit/FormSubmit';
+export * from './component/form/form-text-area/FormTextArea';
+export * from './component/form/form-text/FormText';
 export * from './component/trip-preview-list/TripPreviewList';
 export * from './component/trip-preview/TripPreview';
 export * from './component/trip-view/TripView';
-export * from './component/form/form-container/FormContainer';
